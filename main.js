16cbdabfd3e80533a49e9008a3cde15eec8e3ff3
Coders-King-SSG <71335589+Coders-King-SSG@users.noreply.github.com>
2021-01-30 17:16:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,14 +7,19 @@ function preload() {
 function setup() {
     canvas = createCanvas(800, 450);
     canvas.parent('canvas_div');
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function modelLoaded() {
+    console.log('Model loaded')
 }
 
 function draw() {
     image(bg_img,0,0, 800, 450);
     fill('#ff0');
     textSize(18);
-    text('Dog', 200, 75);
-    text('Cat', 350, 125);
+    text('Dog', 110, 75);
+    text('Cat', 360, 125);
     noFill()
     stroke('#ff0')
     rect(100, 55, 400, 375);
